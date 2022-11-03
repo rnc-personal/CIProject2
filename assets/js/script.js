1d3b376f3d8c432080bbd058c0086743659a99dc
rnc-personal <richard.challenger@protonmail.com>
2022-11-03 16:48:48 +0000

    form/settings setup, minor restyling, daily commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Settings (TO DO)
+
+// const grid = document.getElementById('game-grid')
+
+// let sizeSetting = document.getElementById('settings-form').options
+// let gameSize = document.getElementById('settings-form').options.selectedIndex
+
+// function gameSelected(gameSize) {
+      
+
+//     if (gameSize === 0) {
+//         console.log('easy mode')
+//     } else if (gameSize === 1) {
+//         console.log('normal')
+//     } else if (gameSize === 2) {
+//         console.log('hard mode')
+//     }
+// }
+
