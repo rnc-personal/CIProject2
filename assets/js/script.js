908890abd828a34badcd5b3a1a4fb7f5a5afdcbd
rnc-personal <richard.challenger@protonmail.com>
2022-11-03 18:12:10 +0000

    add random gems + apply attributes. gem imgs add

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,20 @@
 //     }
 // }
 
-// this will be the value fro the options selcted
+// this will be the value from the options selcted - general game settings
 const gameSize = 4
-
 const grid = document.getElementById('game-grid')
 const tiles = []
+
+//The gems appearance can change but for referring to them, generic colors from the gems theme are used
+// define what the other themes gems relate to (e.g 'Santa' = 'Blue)
+
+const gems = [
+    'red',
+    'green',
+    'blue',
+    'yellow'
+]
 
 /**
  * Creates the grid area and gems dynamically when starting the game, based on the settings chosen
@@ -30,6 +39,11 @@ function createGrid() {
     for(let i = 0; i < gameSize * gameSize; i++) {
     const gridItem = document.createElement('div')
     gridItem.classList.add('grid-item')
+    gridItem.setAttribute('draggable', true)
+    gridItem.setAttribute('id', i)
+
+    let gemRandomise = Math.floor(Math.random() * gems.length)
+    gridItem.classList.add(gems[gemRandomise])
     grid.appendChild(gridItem)
     tiles.push(gridItem)
     }
