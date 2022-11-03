18eedc08a4ba1636b96168d97fb92711aa727af3
rnc-personal <richard.challenger@protonmail.com>
2022-11-03 19:11:22 +0000

    add event listeners to gems on drag events

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,38 @@ function createGrid() {
         gridItem.classList.add(gemColors[gemRandomise])
         grid.appendChild(gridItem)
         gemArray.push(gridItem)
+
+        //Add E.L for each gem so we know when the user is dragging it
+        gemArray.forEach(gem => gem.addEventListener('dragstart', dragStart))
+        gemArray.forEach(gem => gem.addEventListener('dragover', dragOver))
+        gemArray.forEach(gem => gem.addEventListener('dragenter', dragEnter))
+        gemArray.forEach(gem => gem.addEventListener('dragleave', dragLeave))
+        gemArray.forEach(gem => gem.addEventListener('dragend', dragEnd))
+        gemArray.forEach(gem => gem.addEventListener('drop', dragDrop))
+
+        function dragStart() {
+            console.log(this.id, 'dragstart')
+        }
+
+        function dragOver() {
+            console.log(this.id, 'dragover')
+        }
+
+        function dragEnter() {
+            console.log(this.id, 'dragenter')
+        }
+
+        function dragLeave() {
+            console.log(this.id, 'dragleave')
+        }
+
+        function dragEnd() {
+            console.log(this.id, 'dragend')
+        }
+
+        function dragDrop() {
+            console.log(this.id, 'drop')
+        }
     }
 }
 
@@ -60,35 +92,11 @@ showHide.addEventListener('click', () => {
 
 //Moving Gems
 
-for (gem of gemArray) {
-    gem.addEventListener('dragstart', dragStart);
-    gem.addEventListener('dragover', dragOver)
-    gem.addEventListener('dragenter', dragEnter)
-    gem.addEventListener('dragleave', dragLeave)
-    gem.addEventListener('dragend', dragEnd)
-    gem.addEventListener('drop', dragDrop)
-}
-
-function dragStart() {
-    console.log((this.id, 'dragstart'))
-}
-
-function dragOver() {
-    console.log((this.id, 'dragover'))
-}
-
-function dragEnter() {
-    console.log((this.id, 'dragenter'))
-}
-
-function dragLeave() {
-    console.log((this.id, 'dragleave'))
-}
-
-function dragEnd() {
-    console.log((this.id, 'dragend'))
-}
-
-function dragDrop() {
-    console.log((this.id, 'drop'))
-}
+// for (let gem of gemArray) {
+//     gem.addEventListener('dragstart', dragStart);
+//     gem.addEventListener('dragover', dragOver)
+//     gem.addEventListener('dragenter', dragEnter)
+//     gem.addEventListener('dragleave', dragLeave)
+//     gem.addEventListener('dragend', dragEnd)
+//     gem.addEventListener('drop', dragDrop)
+// }
